5dc9fc6d6cc02ba827ce40bd19ef65701650b836
ordu0003 <ordu0003@algonquinlive.com>
2023-04-16 19:41:54 -0400

    local storage

--- main.js
+++ main.js
@@ -145,10 +145,26 @@
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        document.querySelector('img.img').src = data.hdurl
+        if( data.media_type === 'image' ){
+            document.querySelector('img.img').src = data.hdurl
+        }else if(data.media_type === 'video'){
+            document.querySelector('iframe').src = data.url
+        }
+        document.querySelector('h3').innerText = data.title
         document.querySelector('h4').innerText = data.explanation
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
+ }
+
+
+ localStorage.setItem('save', 0)
+
+ document.querySelector('.button2').addEventListener('clicl', saveFav)
+
+ function saveFav(){
+    let saveVal = localStorage.getItem('save')
+    saveVal += 'image'
+    localStorage.setItem('save'.saveVal)
  }
